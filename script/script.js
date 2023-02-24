864c9bdee99137b89c245c89123dfb9bdf0735d8
umerovzafar <umerov.99@gmail.com>
2023-02-24 19:51:58 +0500

    stephenbutler

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 let bg = document.querySelector('.header_bg');
 let parallaxBg = document.querySelector('.parallax_bg');
 let header = document.querySelector('.header');
+let navContainer = document.querySelector('.nav_container');
 
 header.onmousemove = (e) => {
   let x = e.clientX / window.innerWidth;
@@ -9,3 +10,8 @@ header.onmousemove = (e) => {
   bg.style.transform = "translate(-" + x * 10 + "px, -" + y * 10 + "px)";
   parallaxBg.style.transform = "translate(-" + x * 10 + "px, -" + y * 10 + "px)";
 }
+
+
+window.addEventListener('load',function() {
+  navContainer.classList.add('active');
+} );
